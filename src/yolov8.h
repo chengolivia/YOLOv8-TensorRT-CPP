9635868d2c9e60fd89f46f81235fa1142fc6de26
daedalus <hfernandez@daed.com>
2025-03-03 11:59:41 -0500

    Add inference mask (160x160) to detected Object

--- src/yolov8.h
+++ src/yolov8.h
@@ -19,6 +19,8 @@ struct Object {
     cv::Mat boxMask;
     // Pose estimation key points
     std::vector<float> kps{};
+    // Added: inference size mask
+    cv::Mat infMask;
 };
 
 // Config the behavior of the YoloV8 detector.
